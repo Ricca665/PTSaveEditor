53180f6065709d529dd978298a7c4fb9995f4b7e
Ricca665 <riccardosarti2010@gmail.com>
2025-05-31 20:35:55 +0200

    modified some stuff to integrate nuitka support

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo "Step 1/2: Installing required libs (dearpygui, py2exe)"
 pip install dearpygui nuitka
 
 echo "Step 2/2: Compiling code into executable..."
-nuitka --standalone --onefile src/main.py
+nuitka --standalone --onefile --output-filename=PTSaveEditor.exe src/main.py
 
 echo "Done! Check for any errors during compilation and/or libs installing"
 pause
