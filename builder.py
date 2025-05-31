00200dd13d07199753be9cb84294f081a0b6758d
Ricca665 <riccardosarti2010@gmail.com>
2025-05-31 20:44:26 +0200

    changed build system

---
+++ builder.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+"""Compiler options"""
+libs = ["dearpygui", "nuitka"]
+compiler = "nuitka"
+compiler_flags = " --standalone --onefile --output-filename=PTSaveEditor.exe src/main.py"
+
+
+print("Step 1/2: installing required libs")
+for lib in libs: # For each library
+    print(f"Installing {lib}...") # Display the current library to install
+    subprocess.run(["pip", "install", lib]) # Installs the library
+
+print("Finished installing libs")
+
+print("Step 2/2: Compiling program")
+print(f"Compiler of choice: {compiler}") # Shows the compiler that is going to be used
+print(f"Compiler flags: {compiler_flags}") # Shows the compiler flags
+print(f"Building with the following command:\n{compiler+compiler_flags}")
+os.system(f"{compiler+compiler_flags}") # Runs the compiler with flags
+
+print("Done! Check for any errors")
