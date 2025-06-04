6b99c67e1a2b2f956efd95a30791e930651108b3
Ricca665 <riccardosarti2010@gmail.com>
2025-06-04 14:42:43 +0200

    FINALLY IT GOT UNWITHELD,  i want to burn avast with fucking fire, let it burn ALL the way down until it remains NOTHING!

--- builder.py
+++ builder.py
@@ -1,16 +1,23 @@
 import os
 import subprocess
 import zipfile
+import shutil
 
 """Compiler options"""
-libs = ["dearpygui", "pyinstaller", "pymsgbox"] # Required libraries 
 compiler = "pyinstaller" # Compiler
 compiler_flags = '--onefile --windowed src/main.py --name="PTSaveFileEditor.exe" --clean --noupx ' \
-                    '--version-file=version.txt' # Compiler flags, version.txt to bypass retarded AVs 🌹🌹
+                    '--version-file=version.txt' # Compiler flags, version.txt to bypass retarded AVs,  mf i have AVs now 🌹🌹
+libs = ["dearpygui", compiler, "pymsgbox"] # Required libraries 
+
 
 version_number = "3" # The version number of the program
 
 os.chdir(os.path.dirname(os.path.realpath(__file__))) # Enters the current directory where the builder is found, this fixes the "Script file 'src/main.py' does not exist." error when compiling
+try:
+    shutil.rmtree("dist")
+    shutil.rmtree("output")
+except:
+    pass
 
 print("Step 1/3: installing required libs")
 for lib in libs: # For each library
@@ -22,13 +29,17 @@ print("Finished installing libs")
 print("Step 2/3: Compiling program")
 print(f"Compiler of choice: {compiler}") # Shows the compiler that is going to be used
 print(f"Compiler flags: {compiler_flags}") # Shows the compiler flags
-print(f"Building with the following command:\n{compiler+compiler_flags}")
+print(f"Building with the following command:\n{compiler} {compiler_flags}")
 os.system(f"{compiler} {compiler_flags}") # Runs the compiler with flags
 
 print("Step 3/3: Zipping program")
-os.chdir("dist")
+try:
+    os.makedirs("output")
+except:
+    pass
+shutil.copyfile("dist/PTSaveFileEditor.exe", "output/PTSaveFileEditor.exe")
+os.chdir("output")
 zip = zipfile.ZipFile(f"PTSaveFileEditorV{version_number}.zip", "w", zipfile.ZIP_DEFLATED)
 zip.write("PTSaveFileEditor.exe")
-zip.close()
 
 print("Done! Check for any errors")
