429cc2c460a652368e138bfdf288f8f6785d964c
Ricca665 <riccardosarti2010@gmail.com>
2025-06-22 15:56:37 +0200

    change the builder yet again...

--- builder.py
+++ builder.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import zipfile
 import shutil
+from builder_utils import *
 
 """Compiler options"""
 compiler = "pyinstaller" # Compiler
@@ -9,37 +10,30 @@ compiler_flags = '--onefile --windowed src/main.py --name="PTSaveFileEditor.exe"
                     '--version-file=version.txt' # Compiler flags, version.txt to bypass retarded AVs,  mf i have AVs now 🌹🌹
 libs = ["dearpygui", compiler, "pymsgbox"] # Required libraries 
 
-
 version_number = "4" # The version number of the program
+def main():
+    os.chdir(os.path.dirname(os.path.realpath(__file__))) # Enters the current directory where the builder is found, this fixes the "Script file 'src/main.py' does not exist." error when compiling
+    try:
+        shutil.rmtree("dist")
+        shutil.rmtree("output")
+    except:
+        pass
 
-os.chdir(os.path.dirname(os.path.realpath(__file__))) # Enters the current directory where the builder is found, this fixes the "Script file 'src/main.py' does not exist." error when compiling
-try:
-    shutil.rmtree("dist")
-    shutil.rmtree("output")
-except:
-    pass
+    print("Step 1/3: installing required libs")
+    install_libs(libs)
+    print("Finished installing libs")
 
-print("Step 1/3: installing required libs")
-for lib in libs: # For each library
-    print(f"Installing {lib}...") # Display the current library to install
-    subprocess.run(["pip", "install", lib]) # Installs the library
+    print("Step 2/3: Compiling program")
+    print(f"Compiler of choice: {compiler}") # Shows the compiler that is going to be used
+    print(f"Compiler flags: {compiler_flags}") # Shows the compiler flags
+    print(f"Building with the following command:\n{compiler} {compiler_flags}")
+    compile(compiler, compiler_flags)
 
-print("Finished installing libs")
+    print("Step 3/3: Zipping program")
+    zipfilepath = f"../output/PTSaveFileEditorV{version_number}.zip"
+    packageExec(zipfilepath)
 
-print("Step 2/3: Compiling program")
-print(f"Compiler of choice: {compiler}") # Shows the compiler that is going to be used
-print(f"Compiler flags: {compiler_flags}") # Shows the compiler flags
-print(f"Building with the following command:\n{compiler} {compiler_flags}")
-os.system(f"{compiler} {compiler_flags}") # Runs the compiler with flags
+    print("Done! Check for any errors")
 
-print("Step 3/3: Zipping program")
-try:
-    os.makedirs("output")
-except:
-    pass
-os.chdir("dist")
-zip = zipfile.ZipFile(f"../output/PTSaveFileEditorV{version_number}.zip", "w", zipfile.ZIP_DEFLATED)
-zip.write("PTSaveFileEditor.exe")
-zip.close()
-
-print("Done! Check for any errors")
+if __name__ == "__main__":
+    main()
---
+++ builder_utils.py
@@ -0,0 +1,31 @@
+import subprocess
+import zipfile
+import os
+import sys
+import shlex
+
+def install_libs(libs):
+    for lib in libs: # For each library
+        print(f"Installing {lib}...") # Display the current library to install
+        subprocess.run([sys.executable, "-m", "pip", "install", lib]) # Installs the library
+    
+def compile(compiler, flags):
+    flags_splitted = shlex.split(flags) # Splits each of the flags
+    subprocess.run([compiler, flags_splitted]) # Compiles the executable
+
+def packageExec(zipfilepath):
+    try:
+        os.makedirs("output")
+    except:
+        pass
+
+    os.chdir("dist")
+    zip = zipfile.ZipFile(zipfilepath, "w", zipfile.ZIP_DEFLATED) # Zips the file
+    zip.write("PTSaveFileEditor.exe")
+    zip.close()
+
+if __name__ == "__main__": # IN case the builder doesn't get invoked
+    import builder
+    print("Builder.py not invoked! Installing neccesary libs...")
+    install_libs(builder.libs)
+    print("Done!")
